7cad00fec7e44362eac8714e4a22b43855d0703f
Chet Gutwein <cgutwein@ischool.berkeley.edu>
2019-08-19 21:27:25 -0600

    Add files via upload
    for Purple Air dashboard

---
+++ mp_dash.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+import psycopg2
+from config import config
+from PA import *
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+ # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+     # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
+
+
+def insert_sensor_data(key):
+    """ insert a new vendor into the vendors table """
+    sql = PA_sensor_pull(key)
+    conn = None
+
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql)
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return print("Data for " + key + "written to database.")
