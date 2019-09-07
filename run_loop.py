4b71d14bc1440c9b1b77f5fba7ecf88761c663d4
cgutwein <cgutwein@ischool.berkeley.edu>
2019-09-07 07:31:54 -0600

    deleting duplicate files

--- run_loop.py
+++
@@ -1,8 +0,0 @@
-from mp_dash import insert_sensor_data
-from PA import master_sensor
-from time import sleep
-
-while True:
-    for key in list(master_sensor.keys()):
-        insert_sensor_data(key)
-    sleep(75)
